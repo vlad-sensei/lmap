5bc726a122d2d86c2015de91ea34e777416cfd48
Vladimir Valyukh <vlava115@student.liu.se>
2012-11-29 19:06:08 +0100

    modified:   algoritm.py

--- algoritm.py
+++ algoritm.py
@@ -4,18 +4,32 @@
 from priodict import priorityDictionary
 
 def Dijkstra(G,start, end=None):
-  D = P = {}
+  D = {}
+  P = {}
   Q = priorityDictionary()
   Q[start] = 0
-
   for v in Q:
     D[v]=Q[v]
     if v==end : break
-
-    for w,e in G:
+    for w,e in G[v]:
       newlen = D[v] + e
-      if w not in Q or newlen < Q[w]:
+      if not w in Q or newlen < Q[w]:
         Q[w]=newlen
         P[w]=v
-
+  #print D[end]
+  #print P
   return(D, P)
+
+def shortestPath(G,start,end):
+  D,P = Dijkstra(G,start,end)
+  Path = []
+  while 1:
+    Path.append(end)
+    if end == start: break
+    end = P[end]
+  Path.reverse()
+  return Path
+
+#G = [[(1,10), (2,30)], [(0,10), (2,10)], [(0,30), (1,10)]]
+#P = shortestPath(G,0,2)
+#print P
