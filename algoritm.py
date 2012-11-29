e29d65858dbe12d51325e973e49f42eb6610102f
Vladimir Valyukh <vlava115@student.liu.se>
2012-11-29 15:14:53 +0100

    modified:   algoritm.py 	modified:   nodes.py 	new file:   priodict.py 	new file:   priodict.pyc

--- algoritm.py
+++ algoritm.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
-import heapq
+# http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/117228
+from priodict import priorityDictionary
 
 def Dijkstra(G,start, end=None):
   D = P = {}
+  Q = priorityDictionary()
+  Q[start] = 0
+
+  for v in Q:
+    D[v]=Q[v]
+    if v==end : break
+
+    for w,e in G:
+      newlen = D[v] + e
+      if w not in Q or newlen < Q[w]:
+        Q[w]=newlen
+        P[w]=v
+
+  return(D, P)
